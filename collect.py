ab3bc2068ad576703a13f4ac3904440f11bb884c
colbyjanecka <colbywho@gmail.com>
2024-03-06 18:46:44 -0800

    changed approach

--- collect.py
+++ collect.py
@@ -1,15 +1,50 @@
-from twilio.rest import Client
-import auth.py
+import auth
+import csv
+import datetime
+import sys
+
+file = open('numbers.csv', 'a')
+file = csv.writer(file)
+
+current_time = datetime.datetime.now()
+
+number = sys.argv[1]
+
+file.writerow([current_time, number])
 
 
-account_sid = 'AC28d031667feaac6108f812d5af5a2709'
-auth_token = auth.token
-client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-  from_='+18443373805',
-  body='Hello from Mania',
-  to='+18777804236'
-)
-
-print(message.sid)
+import smtplib
+import sys
+ 
+CARRIERS = {
+    "att": "@mms.att.net",
+    "tmobile": "@tmomail.net",
+    "verizon": "@vtext.com",
+    "sprint": "@messaging.sprintpcs.com"
+}
+ 
+EMAIL = auth.gmail_username
+PASSWORD = auth.gmail_password
+ 
+def send_message(phone_number, carrier, message):
+    recipient = phone_number + CARRIERS[carrier]
+    auth = (EMAIL, PASSWORD)
+ 
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(auth[0], auth[1])
+ 
+    server.sendmail(auth[0], recipient, message)
+ 
+ 
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print(f"Usage: python3 {sys.argv[0]} <PHONE_NUMBER> <CARRIER> <MESSAGE>")
+        sys.exit(0)
+ 
+    phone_number = sys.argv[1]
+    carrier = sys.argv[2]
+    message = sys.argv[3]
+ 
+    send_message(phone_number, carrier, message)
